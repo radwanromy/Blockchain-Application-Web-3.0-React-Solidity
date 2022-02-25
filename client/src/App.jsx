1f5cdde35495012670e5c306a696f94f3a841d49
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-02-25 23:44:43 +0600

    Blockchain 0011

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import { Navbar, Welcome, Footer, Transactions, Services, Loader } from "./components"
+
 const App = () => {
-
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+
     </div>
   )
 }
