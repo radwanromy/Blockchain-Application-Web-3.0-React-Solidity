5123fd423c5033fdae3194ff526a238f20543308
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-02-26 00:24:02 +0600

    Blockchain 0014

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
+
+import logo from "../../images/logo.png";
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
-
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
 
         </nav>
